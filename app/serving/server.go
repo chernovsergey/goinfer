352019c28dfa5fab8b93042cea88192e2ec3aebc
chernovsergey <chernov.sergey23@gmail.com>
2019-09-27 22:53:57 +0300

    Added model parsing

--- app/serving/server.go
+++ app/serving/server.go
@@ -1,9 +1,13 @@
 package serving
 
 import (
+	"bufio"
 	"context"
 	"log"
 	"net"
+	"os"
+	"strconv"
+	"strings"
 
 	pb "github.com/go-code/goinfer/api"
 	"google.golang.org/grpc"
@@ -28,23 +32,60 @@ func RunServer(config Yaml) *grpc.Server {
 }
 
 type Inferencer struct {
+	features     map[string]bool
+	interactions map[string]bool
+	coef         map[string]float64
 }
 
 func NewInferencer(config Yaml) *Inferencer {
 	path := config["model"].(string)
-	loadModel(path)
-	return &Inferencer{}
+	obj := Inferencer{
+		features:     make(map[string]bool),
+		interactions: make(map[string]bool),
+		coef:         make(map[string]float64),
+	}
+	obj.loadModel(path)
+	return &obj
 }
 
-func loadModel(path string) {
+func (inf *Inferencer) loadModel(path string) {
 	// Todo
 	// - load model
 	// - obtain features and interactions
 	// - obtain coefficients
 	// - save to fast access structure
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatalf("Cant open model file: %s", path)
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		record := scanner.Text()
+		tokens := strings.Split(record, ":")
+
+		// parse featurename=featurevalue format
+		pair := strings.Split(tokens[1], "=")
+		name, _ := pair[0], pair[1]
+
+		if !strings.Contains(tokens[1], "XX") {
+			inf.features[name] = true
+		} else {
+
+			inf.interactions[name] = true
+		}
+
+		coef, err := strconv.ParseFloat(tokens[2], 64)
+		if err != nil {
+			log.Fatalf("Cant parse coefficient %s=%s", tokens[1], tokens[2])
+		}
+		inf.coef[tokens[1]] = coef
+	}
+	log.Printf("Model have load.\nFeatures: %v\nInteractions: %v\nOveral coefficient count: %d",
+		inf.features, inf.interactions, len(inf.coef))
 }
 
 func (inf *Inferencer) PredictProba(c context.Context,
 	req *pb.Request) (*pb.Response, error) {
+	log.Println("Serving request: ", req)
 	return &pb.Response{Proba: 0.0, Confidence: 1.0}, nil
 }
