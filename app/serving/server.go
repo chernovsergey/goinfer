4099d6f1c3cd8cbc901197cc832cbd337c2972d0
chernovsergey <chernov.sergey23@gmail.com>
2019-10-12 21:28:50 +0300

    First failed version of model preprocessing

--- app/serving/server.go
+++ app/serving/server.go
@@ -13,6 +13,66 @@ import (
 	"google.golang.org/grpc"
 )
 
+//revive:disable:exported
+
+type FeatureName uint8
+
+const (
+	FeatureZoneID FeatureName = iota
+	FeatureBannerID
+	FeatureGeo
+	FeatureBrowser
+	FeatureOsVersion
+
+	TotalFeatureCount
+
+	FeatureNameSeparator  = "XX"
+	FeatureValueSeparator = "X~X"
+)
+
+func (f FeatureName) fromRequest(req *pb.Request) {
+	switch f {
+	case FeatureZoneID:
+		req.GetZoneId()
+	}
+}
+
+type FeatureNameString string
+
+var featureNameToString = map[FeatureName]FeatureNameString{
+	FeatureZoneID:    "zone_id",
+	FeatureBannerID:  "banner_id",
+	FeatureGeo:       "geo",
+	FeatureBrowser:   "browser",
+	FeatureOsVersion: "os_version",
+}
+
+var featureNameFromString = make(
+	map[FeatureNameString]FeatureName, TotalFeatureCount,
+)
+
+func initFeatureNameFromString() {
+	for f := FeatureZoneID; f < TotalFeatureCount; f++ {
+		featureNameFromString[featureNameToString[f]] = f
+	}
+}
+
+func (f FeatureName) StringName() FeatureNameString {
+	return featureNameToString[f]
+}
+
+func (f FeatureNameString) IntName() FeatureName {
+	return featureNameFromString[f]
+}
+
+type FeatureValue uint32
+type Coefficient float32
+
+type IndexKey [2]FeatureName
+type IndexValue [2]FeatureValue
+type ValueMap map[IndexValue]Coefficient
+type CoefficientIndex map[IndexKey]ValueMap
+
 type Yaml map[interface{}]interface{}
 
 func RunListener(port string) *net.Listener {
@@ -32,17 +92,18 @@ func RunServer(config Yaml) *grpc.Server {
 }
 
 type Inferencer struct {
-	features     map[string]bool
-	interactions map[string]bool
-	coef         map[string]float64
+	features map[IndexKey]bool
+	coef     CoefficientIndex
 }
 
 func NewInferencer(config Yaml) *Inferencer {
+
+	initFeatureNameFromString()
+
 	path := config["model"].(string)
 	obj := Inferencer{
-		features:     make(map[string]bool),
-		interactions: make(map[string]bool),
-		coef:         make(map[string]float64),
+		features: make(map[IndexKey]bool),
+		coef:     make(CoefficientIndex),
 	}
 	obj.loadModel(path)
 	return &obj
@@ -58,34 +119,82 @@ func (inf *Inferencer) loadModel(path string) {
 	if err != nil {
 		log.Fatalf("Cant open model file: %s", path)
 	}
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		record := scanner.Text()
-		tokens := strings.Split(record, ":")
+		line := scanner.Text()
+		tokens := strings.Split(line, ":")
 
-		// parse featurename=featurevalue format
-		pair := strings.Split(tokens[1], "=")
-		name, _ := pair[0], pair[1]
-
-		if !strings.Contains(tokens[1], "XX") {
-			inf.features[name] = true
-		} else {
-
-			inf.interactions[name] = true
-		}
-
+		// parse coefficient value
 		coef, err := strconv.ParseFloat(tokens[2], 64)
 		if err != nil {
 			log.Fatalf("Cant parse coefficient %s=%s", tokens[1], tokens[2])
 		}
-		inf.coef[tokens[1]] = coef
+
+		// parse featurename=featurevalue value
+		kv := strings.Split(tokens[1], "=")
+		name, value := kv[0], kv[1]
+
+		var mapKey IndexKey
+		var mapVal IndexValue
+		if !strings.Contains(name, "XX") {
+			// simple features e.g. zone_id=12345
+
+			// corner case
+			if value == "fit.other" {
+				continue
+			}
+
+			vint, err := strconv.Atoi(value)
+			if err != nil {
+				log.Fatalf("Cant parse feature %q value to int", value)
+			}
+			mapKey = IndexKey{featureNameFromString[FeatureNameString(name)]}
+			mapVal = IndexValue{FeatureValue(vint)}
+		} else {
+			// double interactions e.g. zone_idXXbanner_id=1234X~X5678
+			nameParts := strings.Split(name, "XX")
+			mapKey = IndexKey{
+				featureNameFromString[FeatureNameString(nameParts[0])],
+				featureNameFromString[FeatureNameString(nameParts[1])],
+			}
+
+			valueParts := strings.Split(value, "X~X")
+			mapVal = IndexValue{}
+			for i, part := range valueParts {
+
+				if part == "fit.other" {
+					continue
+				}
+
+				vint, err := strconv.Atoi(part)
+				if err != nil {
+					log.Fatalf("Cant parse feature %q value to int", value)
+				}
+				mapVal[i] = FeatureValue(vint)
+			}
+		}
+		//log.Println(line)
+		//log.Println(mapKey)
+		//log.Println(mapVal, coef)
+
+		inner, ok := inf.coef[mapKey]
+		if !ok {
+			inner = make(ValueMap)
+			inner[mapVal] = Coefficient(coef)
+			inf.coef[mapKey] = inner
+		} else {
+			inf.coef[mapKey][mapVal] = Coefficient(coef)
+		}
 	}
-	log.Printf("Model have load.\nFeatures: %v\nInteractions: %v\nOveral coefficient count: %d",
-		inf.features, inf.interactions, len(inf.coef))
+
+	log.Printf("Model have loaded")
+	for k, v := range inf.coef {
+		log.Println(k, len(v))
+	}
 }
 
 func (inf *Inferencer) PredictProba(c context.Context,
 	req *pb.Request) (*pb.Response, error) {
-	log.Println("Serving request: ", req)
 	return &pb.Response{Proba: 0.0, Confidence: 1.0}, nil
 }
